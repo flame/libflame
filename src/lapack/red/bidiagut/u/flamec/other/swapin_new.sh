b19528e1c626e71f71b18c6ceb33dafbb026d8ff
Field G. Van Zee <field@cs.utexas.edu>
2015-10-15 12:29:14 -0500

    Removed some old files in 'other' directories.

--- src/lapack/red/bidiagut/u/flamec/other/swapin_new.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp FLA_Fused_Ahx_Axpy_Ax_opt_var1.c.new       FLA_Fused_Ahx_Axpy_Ax_opt_var1.c
-cp FLA_Fused_Gerc2_Ahx_Axpy_Ax_opt_var1.c.new FLA_Fused_Gerc2_Ahx_Axpy_Ax_opt_var1.c
-cp FLA_Fused_Gerc2_opt_var1.c.new             FLA_Fused_Gerc2_opt_var1.c
-cp FLA_Fused_UYx_ZVx_opt_var1.c.new           FLA_Fused_UYx_ZVx_opt_var1.c
-
--- src/lapack/red/hessut/vars/flamec/other/swapin_orig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp FLA_Fused_Ahx_Ax_opt_var1.c.orig        FLA_Fused_Ahx_Ax_opt_var1.c
-cp FLA_Fused_Uhu_Yhu_Zhu_opt_var1.c.orig   FLA_Fused_Uhu_Yhu_Zhu_opt_var1.c
-cp FLA_Fused_Gerc2_Ahx_Ax_opt_var1.c.orig  FLA_Fused_Gerc2_Ahx_Ax_opt_var1.c
