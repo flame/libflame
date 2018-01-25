51516892124fd9d426bdf3045da4d81105903ea7
Field G. Van Zee <field@cs.utexas.edu>
2018-01-25 11:50:46 -0600

    Disable xblas code in lapack2flamec layer.
    Details:
    - Enclosed ?la_*_extended.c files' contents with the cpp macro guard
      #ifdef FLA_ENABLE_XBLAS. (Note that for now, this cpp macro does NOT
      correspond to any configure option.) This change was needed because
      those functions reference extended BLAS symbols that are not present
      in libflame or a standard BLAS library, and thus prevented the
      successful linking of shared library builds of libflame. Thanks to
      Serguei Sokol for pointing out this issue.
    - Added various build products found in the new netlib LAPACK test suite
      to .gitignore so their presence does not clutter up the output of
      'git status' queries.

--- src/map/lapack2flamec/f2c/c/cla_syrfsx_extended.c
+++ src/map/lapack2flamec/f2c/c/cla_syrfsx_extended.c
@@ -1,5 +1,6 @@
 /* ../netlib/cla_syrfsx_extended.f -- translated by f2c (version 20100827). You must link the resulting object file with libf2c: on Microsoft Windows system, link with libf2c.lib;
  on Linux or Unix systems, link with .../path/to/libf2c.a -lm or, if you install libf2c.a in a standard place, with -lf2c -lm -- in that order, at the end of the command line, as in cc *.o -lf2c -lm Source for libf2c is in /netlib/f2c/libf2c.zip, e.g., http://www.netlib.org/f2c/libf2c.zip */
+#ifdef FLA_ENABLE_XBLAS
 #include "FLA_f2c.h" /* Table of constant values */
 static integer c__1 = 1;
 static complex c_b14 =
@@ -810,4 +811,5 @@ L666: /* Set final_* when cnt hits ithresh. */
     }
     return 0;
 }
+#endif
 /* cla_syrfsx_extended__ */
