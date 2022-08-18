76248d05018bee505a503bed38bc1bbc1dfd22c9
Johannes M Dieterich <johannes.dieterich@amd.com>
2022-08-18 17:37:50 -0500

    Correct Copyright.

--- src/base/flamec/wrappers/blas/1/hip/FLA_Copyr_external_hip.c
+++ src/base/flamec/wrappers/blas/1/hip/FLA_Copyr_external_hip.c
@@ -1,6 +1,7 @@
 /*
 
     Copyright (C) 2014, The University of Texas at Austin
+    Copyright (C) 2022, Advanced Micro Devices, Inc.
 
     This file is part of libflame and is available under the 3-Clause
     BSD license, which can be found in the LICENSE file at the top-level
