2728bca1763fce6a5ef59a8688a7cda740600cc7
Parag Sharma <parsharm@amd.com>
2023-01-13 17:56:06 +0530

    Fix for issues reported by Coverity tool
    Fix resource leak and Negative array index read issue in libflame.
    
    Signed-off-by: Parag <parsharm@amd.com>
    AMD-Internal: [CPUPL-2486]
    Change-Id: Ie8ce13f725beb6b01f2ef6e99a82dffe00e202b3

--- src/map/lapack2flamec/f2c/install/util/io/sfe.c
+++ src/map/lapack2flamec/f2c/install/util/io/sfe.c
@@ -35,9 +35,9 @@ integer e_rsfe(void)
 
 int c_sfe(cilist *a)
 {	unit *p;
-	f__curunit = p = &f__units[a->ciunit];
 	if(a->ciunit >= MXUNIT || a->ciunit<0)
 		err(a->cierr,101,"startio");
+	f__curunit = p = &f__units[a->ciunit];
 	if(p->ufd==NULL && fk_open(SEQ,FMT,a->ciunit)) err(a->cierr,114,"sfe")
 	if(!p->ufmt) err(a->cierr,102,"sfe")
 	return(0);
