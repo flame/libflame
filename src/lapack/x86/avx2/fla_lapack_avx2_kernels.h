dfd40259152a72411072ab77a12db834e81e0882
Sridhar Govindaswamy <Sridhar.Govindaswamy@amd.com>
2023-02-16 06:16:03 -0500

    Revert "zgetrf optimization"
    This reverts commit 957aa2f35891c6cea48ccd3f8fe587a9f1f2794b.
    
    Reason for revert: Found accuracy issue for rectangular matrix
    
    Change-Id: Ib29ddf766583a89e7e9a9f7d8dc554eedffc8ef1

--- src/lapack/x86/avx2/fla_lapack_avx2_kernels.h
+++ src/lapack/x86/avx2/fla_lapack_avx2_kernels.h
@@ -12,6 +12,5 @@
 int fla_dhrot3_avx2(integer *n, doublereal *a, integer *lda, doublereal *v, doublereal *tau);
 int fla_drot_avx2(integer *n, doublereal *dx, integer *incx, doublereal *dy, integer *incy, doublereal *c__, doublereal *s);
 int fla_zscal_avx2(integer *n, doublecomplex *alpha, doublecomplex *x, integer *incx);
-int FLA_LU_piv_small_z_avx2( integer *m, integer *n, doublecomplex *a, integer *lda, integer *ipiv, integer *info);
 #endif
 
