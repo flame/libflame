9d854cdfc30409b6caa23f44ace16f3e29dd6524
Jintu Das <jintudas@amd.com>
2023-03-20 05:21:47 -0400

    Fixes NaN and INF inconsistency for QR
    scaling inf with 0 was giving NaN and found to be inconsistent
    with single precision. Added the condition to take BLAS path
    instead of inlined avx2 path for scaling with 0 to keep the outputs
    consistent across precision.
    
    Signed-off-by: Jintu Das <Jintu.Das@amd.com>
    AMD-Internal: SWLCSG-1900
    Change-Id: I4c356100cc5773fbedc589d6a3f3fc98773d7bb7

--- src/lapack/x86/front/fla_lapack_x86_common.h
+++ src/lapack/x86/front/fla_lapack_x86_common.h
@@ -1,5 +1,5 @@
 /******************************************************************************
- * * Copyright (C) 2022, Advanced Micro Devices, Inc. All rights reserved.
+ * * Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
  * *******************************************************************************/
 
 /*! @file fla_lapack_x86_common.c
@@ -11,4 +11,5 @@
 int fla_dhrot3(integer *n, doublereal *a, integer *lda, doublereal *v, doublereal *tau);
 int fla_drot(integer *n, doublereal *dx, integer *incx, doublereal *dy, integer *incy, doublereal *c__, doublereal *s);
 int fla_zscal(integer *n, doublecomplex *alpha, doublecomplex *x, integer *incx);
+int fla_dscal(integer *n, doublereal *da, doublereal *dx, integer *incx);
 #endif
