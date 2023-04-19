dd5021d15b8358199f67d74b738131e51755a25e
prangana <pradeep.rao@amd.com>
2023-04-19 13:08:33 -0700

    Compiler options modifications
    Default compiler options now excludes avx2 and fma flags. These two
    flags are now enabled only for files where avx2 and fma instructions
    are used.
    
    AMD-Internal: CPUPL-2881
    Change-Id: I46f5eb8f328d1216c36e2a32bb38a88b53b8d418

--- src/lapack/x86/avx2/fla_lapack_avx2_kernels.h
+++ src/lapack/x86/avx2/fla_lapack_avx2_kernels.h
@@ -18,20 +18,23 @@ int fla_drot_avx2(integer *n,
                   doublereal *c__, doublereal *s);
 int fla_dscal_ix1_avx2(integer *n, doublereal *da,
                        doublereal *dx, integer *incx);
-int fla_zscal_ix1_avx2(integer *n, doublecomplex *alpha,
-                       doublecomplex *x);
 int fla_dgeqrf_small_avx2(integer *m, integer *n,
                           doublereal *a, integer *lda,
                           doublereal *tau, doublereal *work);
+int fla_dhrot3_avx2(integer *n, doublereal *a, integer *lda, doublereal *v, doublereal *tau);
+int fla_dscal_ix1_avx2(integer *n, doublereal *da, doublereal *dx, integer *incx);
+integer fla_lu_piv_small_d_avx2( integer *m, integer *n, doublereal *a, integer *lda, integer *ipiv, integer *info);
+int fla_sscal_ix1_avx2(integer *n, real *alpha, real *x);
+int fla_sger_avx2(integer *m, integer *n, real *alpha, real *x, integer *incx, real *y, integer *incy, real *a, integer *lda);
 int fla_zgetrf_small_avx2(integer *m, integer *n,
                           doublecomplex *a, integer *lda,
                           integer *ipiv, integer *info);
-int fla_dhrot3_avx2(integer *n, doublereal *a, integer *lda, doublereal *v, doublereal *tau);
-int fla_drot_avx2(integer *n, doublereal *dx, integer *incx, doublereal *dy, integer *incy, doublereal *c__, doublereal *s);
+int fla_zrot_avx2(integer *n, 
+		doublecomplex *cx, integer *incx, 
+		doublecomplex *cy, integer *incy, 
+		doublereal *c__, doublecomplex *s);
 int fla_zscal_avx2(integer *n, doublecomplex *alpha, doublecomplex *x, integer *incx);
-integer fla_lu_piv_small_d_avx2( integer *m, integer *n, doublereal *a, integer *lda, integer *ipiv, integer *info);
-int fla_dscal_ix1_avx2(integer *n, doublereal *da, doublereal *dx, integer *incx);
-int fla_sscal_ix1_avx2(integer *n, real *alpha, real *x);
-int fla_sger_avx2(integer *m, integer *n, real *alpha, real *x, integer *incx, real *y, integer *incy, real *a, integer *lda);
+int fla_zscal_ix1_avx2(integer *n, doublecomplex *alpha,
+                       doublecomplex *x);
 #endif
 
