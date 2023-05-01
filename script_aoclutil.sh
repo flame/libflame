30df5d5ea78f39e0c40c8df3d0278c8d4a35d0cb
prangana <pradeep.rao@amd.com>
2023-05-01 04:47:58 -0400

    Skip AOCL Utils clone on incremental build
    libflame has dependency on libaoclutil project. During build, libflame
    clones AOCL Utils project, builds and links it with libflame binary.
    But, even during incremental build, cloning and build of AOCL Utils
    project was happening. This is now fixed in both Makefile and CMake
    build system. Checks added to see if AOCL Utils build directory exisits
    before cloning the project.
    
    AMD-Internal: CPUPL-3287
    Change-Id: I41839b6f87ab8c3f08022bfe50551e1f5e2f4d13

--- script_aoclutil.sh
+++ script_aoclutil.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Set directory and git paths of libaoclutils library
 LIBAOCLUTILS_DIR=libaoclutils
 LIBAOCLUTILS_OBJ_DIR=libaoclutils_objdir
 LIBAOCLUTILS_STATICLIB=libaoclutils.a
@@ -7,18 +8,30 @@ LIBAOCLUTILS_GIT_URL=git@github.amd.com:AOCL/aocl-utils.git
 LIBAOCLUTILS_GIT_TAG=amd-main
 LIBAOCLUTILS_GIT_REPO=aocl-utils
 
-rm -r $LIBAOCLUTILS_DIR
-mkdir -p $LIBAOCLUTILS_DIR
-rm -r $LIBAOCLUTILS_OBJ_DIR 
-mkdir -p $LIBAOCLUTILS_OBJ_DIR
-cd $LIBAOCLUTILS_DIR
-git clone $LIBAOCLUTILS_GIT_URL -b $LIBAOCLUTILS_GIT_TAG
-cd $LIBAOCLUTILS_GIT_REPO
-mkdir build_temp
-cd build_temp
-cmake ..
-make -j
-cp $LIBAOCLUTILS_STATICLIB ../../../$LIBAOCLUTILS_OBJ_DIR
-cd ../../../$LIBAOCLUTILS_OBJ_DIR
-ar -x $LIBAOCLUTILS_STATICLIB
-cd ..
+#Check of libaoclutils object directory already exists and has object files
+if [[ -d $LIBAOCLUTILS_OBJ_DIR  &&  ! -z `ls $LIBAOCLUTILS_OBJ_DIR/*.o` ]];
+then
+	echo "libaoclutils object files exist."
+else
+	#If object files not found, clone the aoclutils repository
+	#and build it
+	rm -rf $LIBAOCLUTILS_DIR
+	mkdir -p $LIBAOCLUTILS_DIR
+	mkdir -p $LIBAOCLUTILS_OBJ_DIR
+
+	cd $LIBAOCLUTILS_DIR
+	git clone $LIBAOCLUTILS_GIT_URL -b $LIBAOCLUTILS_GIT_TAG
+	
+	cd $LIBAOCLUTILS_GIT_REPO
+	mkdir build_temp
+	cd build_temp
+	cmake ..
+	make -j
+	
+	cp $LIBAOCLUTILS_STATICLIB ../../../$LIBAOCLUTILS_OBJ_DIR
+	cd ../../../$LIBAOCLUTILS_OBJ_DIR
+
+	#Extract the object files
+	ar -x $LIBAOCLUTILS_STATICLIB
+	cd ..
+fi
